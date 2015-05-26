605af9c2c68ee0154a3920b75c1b0cfbd315aad5
Deon Bredenhann <deon@yola.com>
2015-05-26 14:28:55 +0200

    How can I forget setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from setuptools import setup
+import pycloudflare
+
+
+setup(
+    name=pycloudflare.__name__,
+    version=pycloudflare.__version__,
+    description=pycloudflare.__doc__,
+    author='Yola',
+    author_email='engineers@yola.com',
+    url=pycloudflare.__url__,
+    packages=['pycloudflare'],
+    test_suite='nose.collector',
+    install_requires=[
+        'demands >= 1.0.5, < 2.0.0',
+        'yoconfig >= 0.1.0, < 0.2.0',
+    ]
+)
