432ffeb81c5fcab0d10e5900d5b5d2113623dddd
Stefano Rivera <stefano@yola.com>
2015-06-11 16:55:49 -0700

    Methods to create and delete zones

--- pycloudflare/services.py
+++ pycloudflare/services.py
@@ -1,4 +1,5 @@
 from itertools import count
+from urllib import urlencode
 
 from demands import HTTPServiceClient
 from yoconfig import get_config
@@ -12,7 +13,9 @@ class CloudFlareService(HTTPServiceClient):
             'X-Auth-Key': config['api_key'],
             'X-Auth-Email': config['email']
         }
-        super(CloudFlareService, self).__init__(config['url'], headers=headers)
+        self._organization = config.get('organization')
+        super(CloudFlareService, self).__init__(
+            config['url'], headers=headers, send_as_json=True)
 
     def iter_zones(self):
         for page in count():
@@ -28,3 +31,21 @@ class CloudFlareService(HTTPServiceClient):
 
     def get_zone(self, zone_id):
         return self.get('zones/%s' % zone_id).json()['result']
+
+    def get_zone_by_name(self, name):
+        url = 'zones?' + urlencode({'name': name})
+        result = self.get(url).json()['result']
+        assert len(result) <= 1
+        return result[0]
+
+    def create_zone(self, name, jump_start=False):
+        data = {
+            'name': name,
+            'jump_start': jump_start,
+        }
+        if self._organization:
+            data['organization'] = {'id': self._organization}
+        return self.post('zones', data).json()['result']
+
+    def delete_zone(self, zone_id):
+        return self.delete('zones/%s' % zone_id)
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,6 +1,7 @@
 import os
 from unittest import TestCase
 
+from demands import HTTPServiceError
 from yoconfigurator.base import read_config
 from yoconfig import configure_services
 
@@ -19,7 +20,26 @@ class ZonesTest(TestCase):
         zone = next(self.cf.iter_zones())
         self.assertIsInstance(zone, dict)
 
+
+class ZoneTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.cf = CloudFlareService()
+        cls.zone_name = 'example.net'
+        try:
+            zone = cls.cf.create_zone(cls.zone_name)
+        except HTTPServiceError:
+            zone = cls.cf.get_zone_by_name(cls.zone_name)
+        cls.zone_id = zone['id']
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.cf.delete_zone(cls.zone_id)
+
     def test_get_zone(self):
-        zone_id = next(self.cf.iter_zones())['id']
-        zone = self.cloudflare.get_zone(zone_id)
+        zone = self.cf.get_zone(self.zone_id)
         self.assertIsInstance(zone, dict)
+
+    def test_get_zone_by_name(self):
+        zone = self.cf.get_zone_by_name(self.zone_name)
+        self.assertEqual(zone['id'], self.zone_id)
