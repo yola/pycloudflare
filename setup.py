39e9de50042573d10b2599debadda731f0ea5bda
Stefano Rivera <stefano@yola.com>
2015-08-05 11:09:04 -0700

    Include Trove classifiers

--- setup.py
+++ setup.py
@@ -17,5 +17,10 @@ setup(
         'demands >= 1.0.5, < 2.0.0',
         'property-caching >= 1.0.0, < 2.0.0',
         'six >= 1.4.0, < 2.0.0',
-    ]
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
 )
