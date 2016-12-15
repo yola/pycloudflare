9905b8099f302bd9eab6e06edd34a0eb98e7c5fb
Eugene Koval <eugene@yola.com>
2016-12-15 13:15:27 +0200

    Corrected version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## 0.2.4
+## 0.3.1
 
 * Add `services.CloudFlareHostService.zone_set` method.
 
--- pycloudflare/__init__.py
+++ pycloudflare/__init__.py
@@ -1,4 +1,4 @@
 """Python client for CloudFlare."""
 
-__version__ = '0.2.4'
+__version__ = '0.3.1'
 __url__ = 'https://github.com/yola/pycloudflare'
